e67026a05f58924c6b07bab300c0065a3c1159ef
Captain.B <bin@fit2cloud.com>
2019-04-02 15:24:28 +0800

    subscribe broadcast

---
+++ src/puzzlers/article23/extend01.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Button struct {
+	//1
+	Clicked *sync.Cond
+}
+
+func main() {
+
+	lock := &sync.Mutex{}
+	button := Button{Clicked: sync.NewCond(lock)}
+	var box uint
+	subscribe := func(c *sync.Cond, fn func()) { //2
+		go func() {
+			lock.Lock()
+			for box == 0 {
+				c.Wait()
+			}
+			lock.Unlock()
+			fn()
+		}()
+	}
+
+	var wg sync.WaitGroup //3
+	wg.Add(3)
+	subscribe(button.Clicked, func() { //4
+		fmt.Println("Maximizing window.")
+		wg.Done()
+	})
+	subscribe(button.Clicked, func() { //5
+		fmt.Println("Displaying annoying dialog box!")
+		wg.Done()
+	})
+	subscribe(button.Clicked, func() { //6
+		fmt.Println("Mouse clicked.")
+		wg.Done()
+	})
+
+	fmt.Println("before click.")
+	box = 1
+	button.Clicked.Broadcast() //7
+
+	wg.Wait()
+}
